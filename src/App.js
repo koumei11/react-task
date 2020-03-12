fa2dade63c7db73d78745a84e18cd86a95f029eb
KOMEI KINJO <miketyson40166@gmail.com>
2020-03-12 13:15:27 +0900

    props2

--- src/App.js
+++ src/App.js
@@ -2,13 +2,36 @@ import React, {Component} from 'react';
 
 class App extends Component {
   render() {
-  return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={() => console.log("Test")} />
-    </React.Fragment>
-  )
+    const profiles = [
+      {name: "Taro", age: 10},
+      {name: "Hanako", age: 23},
+      {name: "KOMEI"}
+    ]
+    return (
+      <React.Fragment>
+        {
+          profiles.map((user, index) => {
+            return <User name={user.name} age={user.age} key={index}/>
+          })
+        }
+      </React.Fragment>
+    )
   }
+}
+
+class User extends Component {
+  render() {
+    return (
+      <div>
+        {this.props.name}
+        {this.props.age}
+      </div>
+    )
+  }
+}
+
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
